fbff91cb99e86144287ebeed7f9f24c81efc938f
Lili Serfaty <lili.serfaty@gmail.com>
2014-09-26 16:13:10 -0500

    Add google geocoding API to convert all addresses into latlng for mapping.

--- web/server.rb
+++ web/server.rb
@@ -63,46 +63,7 @@ class EffErrands::Server < Sinatra::Application
 
     redirect '/api_request_waypoints'
   end
-  #@@address = {:origins=>["1803+E+18th+Street+Austin+TX", "2300+W+Ben+White+Blvd+Austin+TX", "1000+E+41st+St+Austin+TX+78751"], :destinations=>["2300+W+Ben+White+Blvd+Austin+TX", "1000+E+41st+St+Austin+TX+78751", "1803+E+18th+Street+Austin+TX"]}
- 
-  # USING BEN'S AWESOME ALGORITHM:
-  # get '/api_request' do
-  #   # Make API call to Google:
-  #   en_url = URI.encode('https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + @@address[:origins].join("|") + '&destinations='+ @@address[:destinations].join("|") + '&units=imperial&key=' + ENV['GOOGLE_MAPS_KEY'])
-  #   # Recieve code vomit:
-  #   response = Unirest.get (en_url)
-  #   # Orders code vomit in reasonable JSON:
-  #   data = response.body
-  #   # Empty hash for Ben's data:
-  #   algo_data = []
-  #   i = 0
-  #   # Loop through origin addresses:
-  #   while i < data['origin_addresses'].length
-  #     org_ary = []
-  #     j = 0
-  #     # Loop through destination addresses:
-  #     while j < data['destination_addresses'].length
-  #       dest_ary = []
-  #       # Push each destination into the array:
-  #       dest_ary << data['destination_addresses'][j]
-  #       # Push each destination's distance into the array:
-  #       dest_ary << data['rows'][i]['elements'][j]['distance']['value']
-  #       # Push the distination array into the origins array:
-  #       org_ary << dest_ary
-  #       j += 1
-  #     end
-  #     org_hash = {}
-  #     # Create a hash of origin address keys and their data values:
-  #     org_hash[data['origin_addresses'][i]] = org_ary
-  #     # Push hashes into Ben's array:
-  #     algo_data << org_hash
-  #     i += 1
-  #   end
 
-  # end
-
-  #USING GOOGLE'S SHITTY ALGORITHM:
-  #data['routes'].first['legs'].first['steps'].first['html_instructions']
   get '/api_request_waypoints' do
     # Make API call to google maps: 
     if @@travel_mode == 0
@@ -118,9 +79,12 @@ class EffErrands::Server < Sinatra::Application
     points_hash = {}
     each_stop = []
     all_legs = []
+        address_for_geocoding = []
+
     # Adds all of the addresses into an array in the order sorted by Google:
     data['routes'].first['legs'].each_index do |i|
       each_stop << "#{data['routes'].first['legs'][i]['start_address']}: #{data['routes'].first['legs'][i]['distance']['text']}"
+      address_for_geocoding << data['routes'].first['legs'][i]['start_address'].gsub(/,/, '').gsub(/\s/, '+')
       sub_leg = []
       data['routes'].first['legs'][i]['steps'].each { |x| sub_leg << x['html_instructions']}
       all_legs << sub_leg
@@ -148,8 +112,15 @@ class EffErrands::Server < Sinatra::Application
 
     names.unshift(start_dest.first)
     names.push(end_dest.first)
+    
 
-    binding.pry
+    ## GEOCODING API FOR THE MAP:
+    address_for_geocoding.each do |x|
+      geo_url = URI.encode('https://maps.googleapis.com/maps/api/geocode/json?address=' + x + '&key=' + ENV['GOOGLE_MAPS_KEY'])
+      response = Unirest.get (geo_url)
+      geo_data = response.body
+      geocodes << geo_data['results'].first['geometry']['location']
+    end
 
     erb :route, :locals => {start_dest: start_dest, points: points_hash, names: names, directions: all_legs, end_dest: end_dest, mode: mode}
 
